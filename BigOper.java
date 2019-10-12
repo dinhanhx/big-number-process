e95e2f2a076bbc41ac0868f258fb4ed1ee936ea7
Dinh Anh Vu <38489776+dinhanhx@users.noreply.github.com>
2019-10-12 20:37:45 +0700

    Add files via upload

--- BigOper.java
+++ BigOper.java
@@ -188,4 +188,149 @@ public class BigOper
 
         return outputNumber;
     }
+    public ArrayList<String> multiplication(ArrayList<String> num1, ArrayList<String> num2)
+    {
+        ArrayList<String> outputNumber = new ArrayList<String>();
+        outputNumber.add("0");
+        int n = 0;
+
+        for (int i = 0; i < num1.size(); i++)
+        {
+            for (int j = 0; j < num2.size(); j++)
+            {
+                int sub1 = Integer.parseInt(num1.get(i));
+                int sub2 = Integer.parseInt(num2.get(j));
+                int multiSub = sub1 * sub2;
+                String multiSubtxt = String.format("%d",multiSub);
+                for (int k = 1; k <= i + j; k++)
+                {
+                    multiSubtxt = multiSubtxt.concat("000");
+                }
+                BigNum bnSub = new BigNum();
+                bnSub.setNumtxt(multiSubtxt);
+                outputNumber = addition(outputNumber,bnSub.getNumber());
+            }
+        }
+        return outputNumber;
+    }
+    
+    public boolean comparator(ArrayList<String> num1, ArrayList<String> num2)
+    {
+        boolean num1_is_bigger_than_num2 = false;
+        if (num1.size() > num2.size())
+        { 
+            num1_is_bigger_than_num2 = true;
+            
+        }
+        else
+        {
+            if (num1.size() < num2.size())
+            {
+                num1_is_bigger_than_num2 = false;
+            }
+            else
+            {
+                for (int i = num1.size() - 1; i >= 0; i--)
+                {
+                    int sub1 = Integer.parseInt(num1.get(i));
+                    int sub2 = Integer.parseInt(num2.get(i));
+                    if (sub1 > sub2)
+                    {
+                        num1_is_bigger_than_num2 = true;
+                        break;
+                    }
+                    else
+                    {
+                        if (sub1 < sub2)
+                        {
+                            num1_is_bigger_than_num2 = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return num1_is_bigger_than_num2;
+    }
+
+    public boolean equalChecker(ArrayList<String> num1, ArrayList<String> num2)
+    {
+        boolean isEqual = false;
+        if (num1.size() == num2.size())
+        {
+            for (int i = num1.size() - 1; i >= 0; i--)
+            {
+                int sub1 = Integer.parseInt(num1.get(i));
+                int sub2 = Integer.parseInt(num2.get(i));
+                if (sub1 > sub2)
+                {
+                    isEqual = false;
+                    break;
+                } else
+                    {
+                    if (sub1 < sub2)
+                    {
+                        isEqual = false;
+                        break;
+                    } else {
+                        if (sub1 == sub2)
+                        {
+                            isEqual = true;
+                        }
+                    }
+                }
+            }
+        }
+        else isEqual = false;
+        return isEqual;
+    }
+    
+    public ArrayList<String> division(ArrayList<String> num1, ArrayList<String> num2)
+    {
+        ArrayList<String> outputNumber = new ArrayList<String>();
+        ArrayList<String> R = new ArrayList<String>();
+        ArrayList<String> addOne = new ArrayList<String>();
+        addOne.add("1");
+        if (equalChecker(num1,num2) == true) outputNumber.add("1");
+        else
+        {
+            outputNumber.add("0");
+            if (comparator(num1,num2) == true)
+            {
+                while(true)
+                {
+                    if (comparator(num2,subtraction(num1,multiplication(outputNumber,num2))) == true)
+                    {
+                        R = subtraction(num1,multiplication(outputNumber,num2));
+                        System.out.println(R);
+                        break;
+                    }
+                    outputNumber = addition(outputNumber,addOne);
+                }
+            }
+            else
+            {
+                while(true)
+                {
+                    if (comparator(num1,subtraction(num2,multiplication(outputNumber,num1))) == true)
+                    {
+                        R = subtraction(num2,multiplication(outputNumber,num1));
+                        break;
+                    }
+                    outputNumber = addition(outputNumber, addOne);
+                }
+            }
+        }
+        receiveR(R);
+        return outputNumber;
+    }
+    private ArrayList<String> R = new ArrayList<String>();
+    private void receiveR(ArrayList<String> R)
+    {
+        this.R = R;
+    }
+    public ArrayList<String> returnR()
+    {
+        return R;
+    }
 }
--- DriveCode.java
+++ DriveCode.java
@@ -23,18 +23,39 @@ public class DriveCode
         //Create big operator
         BigOper bo = new BigOper();
 
-        //Create bn3
-        System.out.println();
-        System.out.println("Subtraction of them (Bigger number will subtract smaller one): ");
-        BigNum bn3 = new BigNum();
-        bn3.setNumber(bo.subtraction(bn1.getNumber(),bn2.getNumber()));
-        bn3.displayNumber();
+//        //Create bn3
+//        System.out.println();
+//        System.out.println("Subtraction of them (Bigger number will subtract smaller one): ");
+//        BigNum bn3 = new BigNum();
+//        bn3.setNumber(bo.subtraction(bn1.getNumber(),bn2.getNumber()));
+//        bn3.displayNumber();
+//
+//        //Create bn4
+//        System.out.println();
+//        System.out.println("Addition of them: ");
+//        BigNum bn4 = new BigNum();
+//        bn4.setNumber(bo.addition(bn1.getNumber(),bn2.getNumber()));
+//        bn4.displayNumber();
+//
+//        //Create bn5
+//        System.out.println();
+//        System.out.println("Multiplication of them: ");
+//        BigNum bn5 = new BigNum();
+//        bn5.setNumber(bo.multiplication(bn1.getNumber(),bn2.getNumber()));
+//        bn5.displayNumber();
 
-        //Create bn4
+        //Create bn6
         System.out.println();
-        System.out.println("Addition of them: ");
-        BigNum bn4 = new BigNum();
-        bn4.setNumber(bo.addition(bn1.getNumber(),bn2.getNumber()));
-        bn4.displayNumber();
+        System.out.println("Division of them: ");
+        BigNum bn6 = new BigNum();
+        bn6.setNumber(bo.division(bn1.getNumber(),bn2.getNumber()));
+        bn6.displayNumber();
+
+        //Create bn7
+        System.out.println();
+        System.out.println("R: ");
+        BigNum bn7 = new BigNum();
+        bn7.setNumber(bo.returnR());
+        bn7.displayNumber();
     }
 }
