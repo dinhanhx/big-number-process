cfa5736960d596b98fccfd4aaef5636027895095
Huy Ngo <duchuy29092000@gmail.com>
2019-10-18 13:44:51 +0700

    Fix:

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+	public static void main(String[] args) {
+		// This module is used for testing things automatically
+		// Run this every time something new is implemented.
+
+		int[] op1 = {9392, 564, 7346, 9223, 7142, 8932, 8200, 6048, 6909, 1326};
+		int[] op2 = {3292, 4356, 1674, 5890, 8205, 1094, 7624, 193, 447, 3123};
+
+		int[] res = {30918464, 2456784, 12297204, 54323470, 58600110, 9771608, 62516800, 1167264, 3088323, 4141098};
+
+		BigNum x = new BigNum()
+	}
+}
